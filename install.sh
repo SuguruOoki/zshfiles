66f7e971aa4cdb891c705c91a4794b8b464adbdc
SuguruOoki <oki.work0527@gmail.com>
2018-07-28 17:27:42 +0900

    [追加] tmuxの設定ファイルを追加

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cd ~/zshfiles;
-ln -s ~/zshfiles/.zshrc ~/.zshrc; 
+ln -s ~/zshfiles/.zshrc ~/.zshrc;
+ln -s ~/zshfiles/.tmux.conf ~/.tmux.conf;
 touch .zsh-local
 ln -s ~/zshfiles/.zsh-local ~/.zsh-local
