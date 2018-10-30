85c7ec5d17e40ed70f08a9d09436d5d47ca84771
Suguru Oki <oki.suguru@hamee.co.jp>
2018-10-30 10:35:34 +0900

    ちょっと修正

---
+++ ne/jyuchu_reset.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE `t_trn_jyuchu_header`;
+TRUNCATE TABLE `t_trn_jyuchu_header_simego`;
+TRUNCATE TABLE `t_trn_jyuchu_header_sub`;
+TRUNCATE TABLE `t_trn_jyuchu_meisai`;
+TRUNCATE TABLE `t_trn_jyuchu_meisai_simego`;
+ALTER TABLE `t_trn_jyuchu_header` AUTO_INCREMENT = 1;
+ALTER TABLE `t_trn_jyuchu_header_simego` AUTO_INCREMENT = 1;
+ALTER TABLE `t_trn_jyuchu_meisai` AUTO_INCREMENT = 1;
+ALTER TABLE `t_trn_jyuchu_meisai_simego` AUTO_INCREMENT = 1;
+ALTER TABLE `t_trn_jyuchu_header_sub` AUTO_INCREMENT = 1;
