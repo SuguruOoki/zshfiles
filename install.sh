b694829a93cbe3155b1b2959d5a2f74596f9c7f1
Suguru Oki <oki.suguru@hamee.co.jp>
2018-12-21 10:44:18 +0900

    [修正] gitのファイル系をディレクトリにまとめて移動

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ ln -s ~/zshfiles/.zshrc ~/.zshrc;
 ln -s ~/zshfiles/.tmux.conf ~/.tmux.conf;
 touch .zsh-local
 ln -s ~/zshfiles/.zsh-local ~/.zsh-local
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew file install Brewfile
