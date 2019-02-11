7d1656b89bba19b64805205486dc22805e295eac
Suguru Oki <oki.suguru@hamee.co.jp>
2019-02-11 17:06:53 +0900

    [修正] コード解析用のコマンドをinstall時に使えるように修正

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ ln -s ~/zshfiles/.zsh-local ~/.zsh-local
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew file install Brewfile
 
-// atomを先にinstallしておくと当たり障りない。
+# atomを先にinstallしておくと当たり障りない。
 rm -rf ~/.atom
 ln -nfs ~/zshfiles/.atom ~/.atom
 
+# 権限を与える系
+
+chmod +x ~/zshfiles/code_analysis/cyclic_complexity.sh
